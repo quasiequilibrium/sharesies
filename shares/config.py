a2c9eaf225842ca8ad13e25a36d110a04a47448a
quasiequilibrium <quasixequilibrium@gmail.com>
2024-01-02 11:54:28 +0800

    Add Sharesies CGT Notebook, Sharesies configuration

---
+++ shares/config.py
@@ -0,0 +1,16 @@
+SHARESIES = {
+    'ORDER_ID': 'Order ID',
+    'TRADE_DATE': 'Trade date',
+    'TICKER': 'Instrument code',
+    'MARKET': 'Market code',
+    'QUANTITY': 'Quantity',
+    'PRICE': 'Price',
+    'BUYSELL': 'Transaction type',
+    'EXCHANGE_RATE': 'Exchange rate',
+    'FEE': 'Transaction fee',
+    'CURRENCY': 'Currency',
+    'SHARE_PRICE_TOTAL': 'Amount',
+    'TRANSACTION_METHOD': 'Transaction method',
+    'COMMENTS': 'Comments'
+}
+
